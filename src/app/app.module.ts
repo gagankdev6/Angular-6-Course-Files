85011d5aefcc0cdb18d7c47037d55ebaa2eee256
gagankdev6 <gagank@dev6.com>
2018-07-11 15:40:58 -0400

    starting point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,25 +50,6 @@ export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
 }
 
-const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'signup', component: SignupUserInfoComponent },
-  {
-    path: 'allStyleOptions',
-    loadChildren: 'app/style-gallery/style-gallery.module#StyleGalleryModule',
-    canLoad: [ AuthGuardService ]
-  },
-  {
-    path: 'tshirtsDatabase',
-    loadChildren: 'app/tshirts-database/tshirts-database.module#TShirtsDatabaseModule',
-  },
-  {
-    path: 'catalog', component: CatalogComponent,
-    canActivate: [AuthGuard]
-  },
-  { path: '**', component: HomeComponent}
-];
 
 @NgModule({
   declarations: [
