da59165ffae37a090d214f883c9232837db6f889
gagankdev6 <gagank@dev6.com>
2018-07-12 11:00:46 -0400

    step3

---
+++ src/app/tshirts-database/tshirts-db/tshirts-db.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tshirts-db',
+  templateUrl: './tshirts-db.component.html',
+  styleUrls: ['./tshirts-db.component.css']
+})
+export class TshirtsDbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
